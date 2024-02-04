91ec1fb57193af0d6be0b90885c3de0b3e362488
Magin Luna <magin1104@gmail.com>
2024-02-04 12:14:55 -0400

    * INITIAL PROJECT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'admin',
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./pages/admin/admin.module').then((m) => m.AdminModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
