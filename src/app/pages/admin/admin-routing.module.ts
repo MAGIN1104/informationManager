7464b61db7fce1ad0aaa28fabf881e8682d86bc6
Magin Luna <magin.luna@bancofie.com.bo>
2024-08-25 23:15:54 -0400

    Se actualizo rutas

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     path: '',
     component: AdminComponent,
     children: [
-      { path: '', redirectTo: 'groups', pathMatch: 'full' },
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
       {
         path: 'users',
         loadChildren: () =>
